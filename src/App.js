0059704276a16132621a693afd5a705af0e7fc0f
Manu Varghese <manuvarghese50@gmail.com>
2022-05-29 11:23:16 +0530

    api test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>{
+        axios.get('https://flask-item-rest-api.herokuapp.com/items/array').then((response)=>{
+          console.log(response.data)
+        })
+      }}>Click</button>
     </div>
   );
 }
